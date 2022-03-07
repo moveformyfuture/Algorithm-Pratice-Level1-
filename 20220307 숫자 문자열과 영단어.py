a7f7e76650f27010a959b166757f5e604b56f356
Changjun Park <96476118+moveformyfuture@users.noreply.github.com>
2022-03-07 23:01:04 +0900

    Add files via upload

---
+++ 20220307 숫자 문자열과 영단어.py
@@ -0,0 +1,8 @@
+def solution(s):
+    answer = s
+    num_s = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
+
+    for item in num_s.items(): # items : dic의 쌍 얻기
+        answer = answer.replace(item[0], str(item[1]))
+
+    return int(answer)
